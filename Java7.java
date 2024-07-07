f46ff11eac6949f76bf965e30a4d0cc89c9ffbc6
sohaldhavale96 <sohaldhavale96@gmail.com>
2024-07-07 17:19:54 +0530

    ternary operator

---
+++ Java7.java
@@ -0,0 +1,12 @@
+public class Java7 {
+    public static void main(String[] args) {
+        // ternary operator
+        int num = 19;
+        boolean isEven;
+        isEven = num%2==0 ? true : false;
+        System.out.println(isEven);
+        num = 10;
+        isEven = num%2==0 ? true : false;
+        System.out.println(isEven);
+    }
+}
