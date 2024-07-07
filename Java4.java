b9ff360fe7acab2abcb36ee5e348df8da541f3ef
sohaldhavale96 <sohaldhavale96@gmail.com>
2024-07-07 17:06:57 +0530

    relational operators

---
+++ Java4.java
@@ -0,0 +1,16 @@
+public class Java4 {
+    public static void main(String[] args) {
+        boolean first = (12<13);
+        boolean second = (12>13);
+        boolean third = (12!=13);
+        boolean fourth = (12==13);
+        boolean fifith = (12<=13);
+        boolean sixth = (12>=13);
+        System.out.println(first);
+        System.out.println(second);
+        System.out.println(third);
+        System.out.println(fourth);
+        System.out.println(fifith);
+        System.out.println(sixth);
+    }
+}
