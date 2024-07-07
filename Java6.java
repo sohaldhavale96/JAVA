fddf47da6d4a27778025e7831aa681d35d60c25f
sohaldhavale96 <sohaldhavale96@gmail.com>
2024-07-07 17:17:08 +0530

    if else .  Same as cpp and C

---
+++ Java6.java
@@ -0,0 +1,16 @@
+public class Java6 {
+    public static void main(String[] args) {
+        int age = 19;
+        boolean bringVoterID = true;
+        if(age<18){
+            System.out.println("You are not eligible for voting");
+        }else{
+            System.out.println("You're elgible for voting.");
+            if(bringVoterID){
+                System.out.println("Go and vote");
+            }else{
+                System.out.println("Bring voter ID first and then vote.");
+            }
+        }
+    }
+}
