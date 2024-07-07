90fc01c9e44c74a75bd6a5054a85a275007bfe27
sohaldhavale96 <sohaldhavale96@gmail.com>
2024-07-07 17:02:07 +0530

    Arithmetic operators

---
+++ Java3.java
@@ -0,0 +1,18 @@
+public class Java3 {
+    public static void main(String[] args) {
+        int num1 = 12;
+        int num2 = 45;
+        int add = num1+num2;
+        int sub = num2-num1;
+        int div = num2/num1;
+        int mult = num1*num2;
+        System.out.println(add);
+        System.out.println(sub);
+        System.out.println(mult);
+        System.out.println(div);
+        num1++;
+        System.out.println(num1);
+        num1--;
+        System.out.println(num1);
+    }    
+}
